c2b28a9c2016c213d3361ca3600b7bc421abe95a
Lisxt <alice14072005@gmail.com>
2024-11-25 14:13:50 -0300

    Update Estudante.java

--- Estudante.java
+++ Estudante.java
@@ -1,10 +1,12 @@
-package sistema;
+package Pessoas;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Estudante extends Pessoa {
+	
     private String matricula;
+    
     private static List<Estudante> estudantes = new ArrayList<>(); // Lista estática de estudantes
 
     // Construtor
@@ -31,53 +33,47 @@ public class Estudante extends Pessoa {
     }
     
     // Método para exibir todos os estudantes
-    public static void exibirTodosEstudantes() {
-        for (Estudante estudante : estudantes) {
-            estudante.exibirDados();
+   public static void exibirTodosEstudantes() {
+        for (Estudante est : estudantes) {
+            est.exibirDados();
         }
     }
     
 
     // Método cadastrar estudante
-    public static void cadastrarEstudante(String nome, int idade, String matricula) {
-        Estudante novoEstudante = new Estudante(nome, idade, matricula);
-        estudantes.add(novoEstudante);
-        System.out.println("Estudante " + nome + " cadastrado com sucesso.");
+    public static void cadastrarEstudante(Estudante estudante) {
+        estudantes.add(estudante);
+        System.out.println("Estudante cadastrado com sucesso.");
     }
 
     // Método editar estudante
-    public static void editarEstudante(String nome, String novoNome, int novaIdade, String novaMatricula) {
-        for (Estudante estudante : estudantes) {
-            if (estudante.getNome().equals(nome)) { // Ignora maiúsculas/minúsculas
-                estudante.setNome(novoNome);
-                estudante.setIdade(novaIdade);
-                estudante.setMatricula(novaMatricula);
-                System.out.println("Dados do estudante " + nome + " editados com sucesso.");
+    public static void editarEstudante(String nome, String novoNome, int novaIdade) {
+        for (Estudante est : estudantes) {
+            if (est.getNome().equals(nome)) {
+                est.setNome(novoNome);
+                est.setIdade(novaIdade);
+                System.out.println("Dados do estudante " + nome + " foram editados.");
                 return;
             }
         }
-        System.out.println("Estudante com o nome " + nome + " não encontrado.");
+        System.out.println("Estudante " + nome + " não foi encontrado.");
     }
 
     // Método excluir estudante
     public static void excluirEstudante(String nome) {
-        for (int i = 0; i < estudantes.size(); i++) {
-            Estudante estudante = estudantes.get(i);
-            if (estudante.getNome().equalsIgnoreCase(nome)) { // Ignora maiúsculas/minúsculas
-                estudante.exclusao();
-                estudantes.remove(i);
-                System.out.println("Estudante " + nome + " excluído com sucesso.");
+        for (Estudante est : estudantes) {
+            if (est.getNome().equals(nome)) {
+                est.exclusao();
+                estudantes.remove(est);
+                System.out.println("Estudante " + nome + " foi excluído.");
                 return;
             }
         }
-        System.out.println("Estudante com o nome " + nome + " não encontrado.");
+        System.out.println("Estudante " + nome + " não foi encontrado.");
     }
    // Método exclusão
         public void exclusao() {
-            setNome(null);
-            setIdade(0);
-            setMatricula(null);
-            System.out.println("Dados do estudante excluídos com sucesso!");
+            setNome(null); setIdade(0); setMatricula(null);
+            System.out.println("Dados do estudante foram excluídos com sucesso!");
         }
 
-}
